782ba169d38150ba14d3e340a3379b2840d1265e
care0hlyn <ckhuynh20@gmail.com>
2014-07-20 13:28:35 -0700

    Second commit, jQuery works, refactoring to include prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,95 @@
 
 
 
+
+
+
+
+
+
+
+  // var triangle = { sideOne: side1,
+  //                   sideTwo: side2,
+  //                   sideThree: side3,
+  //                   calc: function () {
+  //                 if ((((this.sideOne + this.sideTwo) < this.sideThree) || ((this.sideTwo + this.sideThree) < this.sideOne) || ((this.sideThree + this.sideTwo) < this.sideOne))) {
+  //                 return "NOT A TRIANGLE";
+  //               } else if (this.sideOne === this.sideTwo && this.sideOne === this.sideThree) {
+  //                 return "EQUILATERAL";
+  //               } else if (this.sideOne === this.sideThree || this.sideTwo === this.sideOne || this.sideThree === this.sideTwo) {
+  //                 return "ISOCELES";
+  //               } else if ((this.sideOne !== this.sideTwo && this.sideOne !== this.sideThree) && (this.sideTwo !== this.sideThree)) {
+  //                 return "SCALENE";
+  //               } else {
+  //                 return false;
+  //               }
+  //               }
+  //             }
+
+
 $(document).ready(function(){
   $('form#form-group').submit(function(event){
     event.preventDefault();
 
-    var sideOne = parseInt($('input#side-one').val());
-    var sideTwo = parseInt($('input#side-two').val());
-    var sideThree = parseInt($('input#side-three').val());
+
+var side1 = parseInt($('input#side-one').val());
+var side2 = parseInt($('input#side-two').val());
+var side3 = parseInt($('input#side-three').val());
+
+
+var triangle = { sideOne: side1,
+                 sideTwo: side2,
+                 sideThree: side3,
+                 type: function () {
+                  if ((((this.sideOne + this.sideTwo) < this.sideThree) || ((this.sideTwo + this.sideThree) < this.sideOne) || ((this.sideThree + this.sideTwo) < this.sideOne))) {
+                  return "NOT A TRIANGLE";
+                } else if (this.sideOne === this.sideTwo && this.sideOne === this.sideThree) {
+                  return "EQUILATERAL";
+                } else if (this.sideOne === this.sideThree || this.sideTwo === this.sideOne || this.sideThree === this.sideTwo) {
+                  return "ISOCELES";
+                } else if ((this.sideOne !== this.sideTwo && this.sideOne !== this.sideThree) && (this.sideTwo !== this.sideThree)) {
+                  return "SCALENE";
+                } else {
+                  return false;
+                  }
+                 } // ending bracket for the function
+              } // ending bracket for the entire object
 
 
 
-  });
-});
+ // var triangle = { sideOne: side1,
+//                  sideTwo: side2,
+//                  sideThree: side3,
+//                  type: function () {
+//                  if (((this.sideOne + this.sideTwo) < this.sideThree) || ((this.sideTwo + this.sideThree) < this.sideOne) || ((this.sideOne + this.sideThree) < this.sideTwo)) {
+//                   if ((this.sideOne === this.sideTwo) && (this.sideOne === this.sideThree)) {
+//                     return "EQUILATERAL";
+//                   } else if (this.sideOne === this.sideTwo || this.sideOne === this.sideThree || this.sideTwo === this.sideThree || this.sideTwo) {
+//                     return "ISOCELES";
+//                   } else if ((this.sideOne !== this.sideTwo) && (this.sideTwo !== this.sideThree) && (this.sideTwo !== this.sideThree)) {
+//                     return "SCALENE";
+//                   } else {
+//                     return false;
+//                     } // ending bracket for the nested IF statement
+//                   } // ending bracket for the first IF statement
+//                  } // ending bracket for the function
+//               } // ending bracket for the entire object
+
+
+
+    if (triangle.type() === "NOT A TRIANGLE") {
+      alert("Not a triangle!");
+    } else if(triangle.type() === "EQUILATERAL") {
+      return $('#equal').append("<li>" + side1 + side2 + side3 + "</li>");
+    } else if(triangle.type() === "ISOCELES") {
+      return $('#two-sides').append("<li>" + side1 + side2 + side3 + "</li>");
+    } else if (triangle.type() === "SCALENE") {
+      return $("#none").append("<li>" + side1 + side2 + side3 + "</li>")
+    } else {
+      return false;
+    }
+
+
+
+  }); //This closes submit button function
+}); // This closes jQuery
