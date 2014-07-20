2cc51e0ed493fdd901cb0caabaf466222ac84791
Paul Ogawa <student@epicodus.com>
2014-07-20 12:28:59 -0700

    First commit, HTML skeleton built

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+
+
+$(document).ready(function(){
+  $('form#form-group').submit(function(event){
+    event.preventDefault();
+
+    var sideOne = parseInt($('input#side-one').val());
+    var sideTwo = parseInt($('input#side-two').val());
+    var sideThree = parseInt($('input#side-three').val());
+
+
+
+  });
+});
