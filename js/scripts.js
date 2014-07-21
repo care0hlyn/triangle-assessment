b75876234d0045e2d14acb8bddc1caf54ad44389
care0hlyn <ckhuynh20@gmail.com>
2014-07-20 17:16:15 -0700

    Fourth commit, first prototype done

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-
-
-
-
+var Greeting = {
+  sayHello: function() {
+    alert("Hello, my name is " + this.name + "!" );
+  }
+};
 
 
 
@@ -15,6 +16,9 @@ var side1 = parseInt($('input#side-one').val());
 var side2 = parseInt($('input#side-two').val());
 var side3 = parseInt($('input#side-three').val());
 
+parseInt($('input#side-one').val(""));
+parseInt($('input#side-two').val(""));
+parseInt($('input#side-three').val(""));
 
 var triangle = { sideOne: side1,
                  sideTwo: side2,
@@ -32,8 +36,7 @@ var triangle = { sideOne: side1,
                   return false;
                   }
                  } // ending bracket for the function
-              } // ending bracket for the entire object
-
+              }; // ending bracket for the entire object
 
 
     if (triangle.type() === "NOT A TRIANGLE") {
@@ -46,9 +49,38 @@ var triangle = { sideOne: side1,
       return $("#none").append("<li>" + side1 + ", " + side2 + ", " + side3 + "</li>")
     } else {
       return false;
-    }
-
-
-
+    };
   }); //This closes submit button function
+
+
+
+  $('#one').click(function() {
+  var equilateral = Object.create(Greeting);
+    equilateral.name = "equilateral";
+    equilateral.sayHello();
+
+  }); //This is to make the equilateral greeting pop up
+
+
+  $('#two').click(function() {
+  var isoceles = Object.create(Greeting);
+    isoceles.name = "isoceles";
+    isoceles.sayHello();
+
+  });
+
+  $('#three').click(function() {
+  var scalene = Object.create(Greeting);
+    scalene.name = "scalene";
+    scalene.sayHello();
+
+  });
+
+
+
+
+
+
+
+
 }); // This closes jQuery
